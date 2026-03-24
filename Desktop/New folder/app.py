8e7ae8e04ca1e44031c85af718090973c2ebf64b
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-03-24 11:22:20 +0530

    qr-project

---
+++ Desktop/New folder/app.py
@@ -0,0 +1,14 @@
+import qrcode
+
+url = input("Enter the URL: ").strip()
+file_path = "C:\\Users\\radha\\Desktop\\New folder\\qrcode.png"
+
+qr = qrcode.QRCode()
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR generated")
+
+print("hello world")
